745086718c8986ede86efbf658603fb8b1354b7b
gomeow <davismariotti@gmail.com>
2016-11-20 18:06:00 -0800

    Fix regex on url patterns

--- person/urls.py
+++ person/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^imageupload$', views.imageupload, name='imageupload'),
+    url(r'^imageupload/', views.imageupload, name='imageupload'),
     url(r'^avatar/', views.avatar, name='avatar'),
     url(r'^(?P<person>[-\w]+)/$', views.idprovided, name='id'),
 ]
